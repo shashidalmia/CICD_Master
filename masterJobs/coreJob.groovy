0791c38da192affbf18a1f53ef30a60204e7ce18
Shashi <shashi.marwadi@gmail.com>
2019-05-12 20:42:06 -0500

    First Commit for Master Jobs

---
+++ masterJobs/coreJob.groovy
@@ -0,0 +1,31 @@
+job('CICD/seed') {
+  
+  parameters {
+
+    stringParam('ARTIFACT_ID', '', '')
+    stringParam('CICD_VERSION', '', '')
+    stringParam('NEXUS_REPO', '', '')
+    stringParam('FOLDER_PREFIX', '', '')
+    stringParam('RELEASE_JOB', '', '')
+    stringParam('DRY_RUN', '', '')
+
+/*    validatingStringParameterDefinition {
+      name 'ABC'
+      regex '^[1-9]*$'
+      defaultValue ''
+      failedValidationMessage 'Invalid Number'
+      description 'enter number'
+    }*/
+  }
+
+    wrappers {
+        preBuildCleanup()
+    }
+
+     environmentVariables {
+            env('GROUP_ID', 'com.shashi.cicd')
+            env('ARTIFACT_VERSION', ${CICD_VERSION})
+            env('NEXUS_URL', 'http://nexus:8080')
+        }
+
+}
